7ed35f7545f809a865ab102a8b7921a96d6599e9
Hailan Wang <hailan6257@gmail.com>
2022-02-03 16:11:52 -0500

    fix/admin-basic-auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with :name => ENV['AUTHENTICATION_USERNAME'], :password => ENV['AUTHENTICATION_PASSWORD']
   def show
   end
 end
