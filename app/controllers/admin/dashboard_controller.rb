eb1a37bb0d19c4ae2575f6efd6d49b7697ea38e9
Hailan Wang <hailan6257@gmail.com>
2022-02-03 20:00:53 -0500

    Add feature/dashboard-counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['AUTHENTICATION_USERNAME'], :password => ENV['AUTHENTICATION_PASSWORD']
   def show
+    @products = Product.all.order(created_at: :desc)
+    @counts = @products.count
+    @categories = Category.all
+    @c_count = @categories.count
   end
+
 end
