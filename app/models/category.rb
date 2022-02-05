a5119f0420c3e25212436c689c67e03a6ccac527
Hailan Wang <hailan6257@gmail.com>
2022-02-05 15:54:37 -0500

    feature/admin-categories done

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
 
   has_many :products
 
+  validates :name, presence: true
+  validates :created_at, presence: false
+  validates :updated_at, presence: false
+
 end
